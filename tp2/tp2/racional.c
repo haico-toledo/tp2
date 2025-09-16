837cd280730bfff342abff5165c35ee8782a5dce
haico-toledo <htb25@inf.ufpr.br>
2025-09-16 01:43:15 -0300

    primeira alteracao tp2

---
+++ tp2/tp2/racional.c
@@ -0,0 +1,85 @@
+/* 
+ * Tipos Abstratos de Dados - TADs
+ * Arquivo de implementação para TAD racional.
+ * Feito em 20/09/2024 para a disciplina CI1001 - Programação 1.
+ *
+ * Este arquivo deve conter as implementações das funções cujos protótipos
+ * foram definidos em racional.h. Neste arquivo também podem ser definidas
+ * funções auxiliares para facilitar a implementação daquelas funções.
+*/
+
+/* coloque aqui seus includes (primeiro os <...>, depois os "...") */
+#include <stdio.h>
+#include <stdlib.h>
+#include "racional.h"
+
+/*
+ * Implemente aqui as funcoes definidas no racionais.h; caso precise,
+ * pode definir aqui funcoes auxiliares adicionais, que devem ser usadas
+ * somente neste arquivo.
+*/
+
+/* retorna um número aleatório entre min e max, inclusive. */
+long aleat (long min, long max)
+{
+	return min + rand() % (max - min + 1);
+}
+
+/* Máximo Divisor Comum entre a e b      */
+/* calcula o MDC pelo método de Euclides */
+long mdc (long a, long b)
+{
+	long resto, aux;
+	
+	if (b > a)
+	{
+		aux = a;
+		a = b;
+		b = aux;
+	}
+		
+	while (b != 0)
+	{
+		resto = a % b;
+		a = b;
+		b = resto;
+	}
+	
+	return a;
+}
+
+/* Mínimo Múltiplo Comum entre a e b */
+/* mmc = (a * b) / mdc (a, b)        */
+long mmc (long a, long b)
+{
+	return ( (a * b) / mdc(a, b) );
+}
+
+/* Recebe um número racional e o simplifica.
+ * Por exemplo, ao receber 10/8 deve retornar 5/4.
+ * Se ambos numerador e denominador forem negativos, deve retornar um positivo.
+ * Se o denominador for negativo, o sinal deve migrar para o numerador.
+ * Se r for inválido, devolve-o sem simplificar. */
+struct racional simplifica_r (struct racional r)
+{
+	long aux = mdc(r.num, r.den);
+	
+	if (! valido_r(r))
+		return r;
+		
+	if (mdc(r.num, r.den) != 1)
+	{
+		r.num =  r.num / aux;
+		r.den = r.den / aux;
+	}
+	
+	if (r.den < 0) 
+	{
+		r.num = (- r.num);
+		r.den = (- r.den);
+	}
+	
+	return r;
+}
+
+/* implemente as demais funções de racional.h aqui */
---
+++ tp2/tp2/testes2.c
@@ -0,0 +1,226 @@
+/*
+ *	testes para o tp2 de prog1 
+ *	ultima alteracao em 15/09/2025
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "racional.h"
+
+/* estrutura que representa um número racional */
+struct racional {
+  long num;          /* numerador   */
+  long den;          /* denominador */
+};
+
+
+long aleat (long min, long max)
+{
+	return min + rand() % (max - min + 1);
+}
+
+
+/* Máximo Divisor Comum entre a e b      */
+/* calcula o MDC pelo método de Euclides */
+long mdc (long a, long b)
+{
+	long resto, aux;
+	
+	if (b > a)
+	{
+		aux = a;
+		a = b;
+		b = aux;
+	}
+		
+	while (b != 0)
+	{
+		resto = a % b;
+		a = b;
+		b = resto;
+	}
+	
+	return a;
+}
+
+
+/* Mínimo Múltiplo Comum entre a e b */
+/* mmc = (a * b) / mdc (a, b)        */
+long mmc (long a, long b)
+{
+	return ( (a * b) / mdc(a, b) );
+}
+
+
+/* Recebe um número racional e o simplifica.
+ * Por exemplo, ao receber 10/8 deve retornar 5/4.
+ * Se ambos numerador e denominador forem negativos, deve retornar um positivo.
+ * Se o denominador for negativo, o sinal deve migrar para o numerador.
+ * Se r for inválido, devolve-o sem simplificar. */
+struct racional simplifica_r (struct racional r)
+{
+	long aux = mdc(r.num, r.den);
+	
+	if (! valido_r(r))
+		return r;
+		
+	if (mdc(r.num, r.den) != 1)
+	{
+		r.num =  r.num / aux;
+		r.den = r.den / aux;
+	}
+	
+	if (r.den < 0) 
+	{
+		r.num = (- r.num);
+		r.den = (- r.den);
+	}
+	
+	return r;
+}
+
+
+/* funcoes do racional.h */
+
+long numerador_r (struct racional r)
+{
+	return r.num;
+}
+
+
+long denominador_r (struct racional r)
+{
+	return r.den;
+}
+
+
+struct racional cria_r (long numerador, long denominador)
+{
+	struct racional r;
+	
+	r.num = numerador;
+	r.den = denominador;
+	
+	return r;
+}
+
+
+int valido_r (struct racional r) 
+{
+	if (r.den == 0)
+		return 0;
+	else
+		return 1;
+}
+
+
+struct racional sorteia_r (long min, long max)
+{
+	struct racional r;
+	
+	r.num = aleat(min, max); 
+	r.den = aleat(min, max); 
+	
+	return simplifica_r(r);
+}
+
+
+void imprime_r (struct racional r)
+{
+	r = simplifica_r(r);
+	
+	if (! valido_r(r))
+	{
+		printf("NaN");
+		return;
+	}
+	
+	if (r.num == 0)
+	{
+		printf("0");
+		return;
+	}
+	
+	if (r.den == 1)
+	{
+		printf("%ld", r.num);
+		return;
+	}
+	
+	if (r.num == r.den)
+	{
+		printf("1");
+		return;
+	}
+	printf("%ld/%ld", r.num, r.den);
+	
+	return;
+}
+
+
+int compara_r (struct racional r1, struct racional r2)
+{
+	if ( (! valido_r(r1)) || (! valido_r(r2)) )
+		return -2;
+		
+	//para comparar coloco os dois na mesma base
+	r1.num = (r1.num * r2.den) / mdc(r1.den, r2.den);
+	r2.num = (r2.num * r1.den) / mdc(r1.den, r2.den);
+	r1.den = mmc(r1.den, r2.den);
+	r2.den = mmc(r1.den, r2.den);
+	
+	if (r1.num < r2.num )
+		return -1;
+	if (r1.num > r2.num)
+		return 1;
+		
+	//retorna 0 se forem iguais	
+	return 0;	
+}
+
+
+int soma_r (struct racional r1, struct racional r2, struct racional *r3)
+{
+	if ( ((! valido_r(r1)) || (! valido_r(r2))) || (r3 == NULL))
+		return 0;
+		
+	 r3 -> num = ((r1.num * r2.den) + (r1.den * r2.num)) / mdc(r1.den, r2.den);   
+	 r3 -> den = mmc(r1.den, r2.den);
+	
+	return 1;
+}	
+
+
+int subtrai_r (struct racional r1, struct racional r2, struct racional *r3)
+{
+	if ( ((! valido_r(r1)) || (! valido_r(r2))) || (r3 == NULL))
+		return 0;
+		
+	 r3 -> num = ((r1.num * r2.den) - (r1.den * r2.num)) / mdc(r1.den, r2.den);   
+	 r3 -> den = mmc(r1.den, r2.den);
+	
+	return 1;
+}
+
+
+
+
+
+int main ()
+{
+	long a = 0, b = 4, c = 7, d = 8;
+	struct racional r1, r2, r3;
+	struct racional *ptr_r3;
+	ptr_r3 = &r3;
+	srand(0);
+	r1 = sorteia_r(a, b);
+	r2 = sorteia_r(c, d);
+	imprime_r(r1);
+	printf("\n");
+	imprime_r(r2);
+	printf("\n%d\n", soma_r(r1, r2, ptr_r3));
+	imprime_r(r3);
+	printf("\n%d\n", subtrai_r(r1, r2, ptr_r3));
+	imprime_r(r3);
+}
+
